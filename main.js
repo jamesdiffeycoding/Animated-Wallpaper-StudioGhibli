9d1d4fca2dbfb780872a2c456c61219ce06afdb8
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-02-06 22:59:59 +0000

    convert vanilla project to React and modernise code

--- main.js
+++
@@ -1,8 +0,0 @@
-const sectiontohide = document.getElementById('navbar');
-const toggleButton = document.getElementById('toggleButton');
-
-toggleButton.addEventListener('click', function() {
-  sectiontohide.classList.toggle('hidden');
-  if (toggleButton.textContent=="Display links") {
-    toggleButton.textContent="Hide links and audio";
-  } else {toggleButton.textContent="Display links"}})
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <video src="/video.mp4" className="hero" autoPlay controls loop muted />
+  );
+}
+
+export default App;
