eacf57a93d47cbad87917a86ff2bd5080df844de
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-02-07 17:55:19 +0000

    refactor with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,65 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [playing, setPlaying] = useState(false);
   const btnRef = useRef<HTMLButtonElement | null>(null);
   const vidRef = useRef<HTMLVideoElement | null>(null);
 
-  function transitionInitialOpacities() {
-    setTimeout(() => {
-      if (btnRef.current && vidRef.current) {
-        btnRef.current.style.opacity = "1";
-        vidRef.current.style.opacity = "0.5";
-        vidRef.current.style.zIndex = "10";
+  function fadeInAudio(increment: number, msInterval: number) {
+    let vol = 0;
+    const interval = setInterval(() => {
+      if (vol < 1 - increment && vidRef.current) {
+        vol += increment;
+        vidRef.current.volume = vol;
+      } else {
+        clearInterval(interval);
       }
-    }, 100);
-  }
-
-  function transitionUIForVideo() {
-    if (btnRef.current && vidRef.current) {
-      btnRef.current.click;
-      btnRef.current.style.cursor = "auto";
-      btnRef.current.style.opacity = "0";
-      vidRef.current.style.opacity = "1";
-      vidRef.current.style.zIndex = "1";
-    }
-    setTimeout(() => {
-      if (btnRef.current) {
-        btnRef.current.style.display = "none";
-      }
-    }, 3000);
+    }, msInterval);
   }
 
   function startVideo() {
-    if (vidRef.current) {
-      vidRef.current.volume = 0;
-      setTimeout(() => {
-        vidRef.current?.play();
-        fadeInAudio();
-      }, 4000);
-      vidRef.current.muted = false;
-    }
-  }
-
-  function fadeInAudio() {
-    let vol = 0;
-    const interval = setInterval(() => {
-      if (vidRef.current) {
-        if (vol < 0.95) {
-          vol += 0.08;
-          vidRef.current.volume = vol;
-        } else {
-          clearInterval(interval);
-        }
+    if (!playing) {
+      setPlaying(true);
+      if (btnRef.current && vidRef.current) {
+        vidRef.current.classList.replace("opacity-30", "opacity-100");
       }
-    }, 400);
-  }
-
-  transitionInitialOpacities();
-
-  function enableControls() {
-    if (vidRef.current) {
-      vidRef.current.controls = true;
+      setTimeout(() => {
+        if (vidRef.current) {
+          vidRef.current.play();
+          vidRef.current.volume = 0;
+          fadeInAudio(0.05, 100);
+        }
+      }, 3000);
     }
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (btnRef.current && vidRef.current) {
+        btnRef.current.classList.replace("opacity-0", "opacity-100");
+        vidRef.current.classList.replace("opacity-0", "opacity-30");
+      }
+    }, 1000);
+  }, []);
 
   return (
     <>
       <video
+        className="relative w-full h-screen object-cover opacity-0"
         ref={vidRef}
         aria-label="studio-ghibli-animation"
-        onClick={enableControls}
+        onClick={() => {
+          if (vidRef.current) vidRef.current.controls = true;
+        }}
         src="/video.mp4"
-        className="animated-video"
+        muted={!playing}
         loop
       />
       <button
         ref={btnRef}
-        className="start-btn ghibli-heading-font"
-        onClick={() => {
-          transitionUIForVideo();
-          startVideo();
-        }}
+        className={`custom-ghibli-gif custom-ghibli-font custom-ghibli-position-size opacity-0 bg-gray-500 border-2 border-white
+          ${playing ? "opacity-0" : "cursor-pointer"}`}
+        onClick={startVideo}
       >
         STEP OUTSIDE
       </button>
